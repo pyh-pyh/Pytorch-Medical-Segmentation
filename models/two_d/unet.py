5d41e235bf919c8643c63bce13fada0ff1c51276
pyh-pyh <2335257900@qq.com>
2021-11-30 15:49:17 +0800

    formatting

--- models/two_d/unet.py
+++ models/two_d/unet.py
@@ -1,6 +1,5 @@
 # 2D-Unet Model taken from https://github.com/milesial/Pytorch-UNet/blob/master/unet/unet_model.py
 
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -9,16 +8,12 @@ import os
 
 class DoubleConv(nn.Module):
     '''(conv => BN => ReLU) * 2'''
-
     def __init__(self, in_ch, out_ch):
         super(DoubleConv, self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, 3, padding=1),
-            nn.BatchNorm2d(out_ch),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_ch, out_ch, 3, padding=1),
-            nn.BatchNorm2d(out_ch),
-            nn.ReLU(inplace=True))
+        self.conv = nn.Sequential(nn.Conv2d(in_ch, out_ch, 3, padding=1),
+                                  nn.BatchNorm2d(out_ch), nn.ReLU(inplace=True),
+                                  nn.Conv2d(out_ch, out_ch, 3, padding=1),
+                                  nn.BatchNorm2d(out_ch), nn.ReLU(inplace=True))
 
     def forward(self, x):
         x = self.conv(x)
@@ -38,10 +33,7 @@ class InConv(nn.Module):
 class Down(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(Down, self).__init__()
-        self.mpconv = nn.Sequential(
-            nn.MaxPool2d(2),
-            DoubleConv(in_ch, out_ch)
-        )
+        self.mpconv = nn.Sequential(nn.MaxPool2d(2), DoubleConv(in_ch, out_ch))
 
     def forward(self, x):
         x = self.mpconv(x)
@@ -53,7 +45,9 @@ class Up(nn.Module):
         super(Up, self).__init__()
 
         if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.up = nn.Upsample(scale_factor=2,
+                                  mode='bilinear',
+                                  align_corners=True)
         else:
             self.up = nn.ConvTranspose2d(in_ch // 2, in_ch // 2, 2, stride=2)
 
@@ -65,8 +59,9 @@ class Up(nn.Module):
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
 
-        x1 = F.pad(x1, (diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2))
+        x1 = F.pad(
+            x1,
+            (diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2))
         x = torch.cat([x2, x1], dim=1)
         x = self.conv(x)
         return x
@@ -86,7 +81,7 @@ class Unet(nn.Module):
     def __init__(self, in_channels, classes):
         super(Unet, self).__init__()
         self.n_channels = in_channels
-        self.n_classes =  classes
+        self.n_classes = classes
 
         self.inc = InConv(in_channels, 64)
         self.down1 = Down(64, 128)
@@ -98,7 +93,6 @@ class Unet(nn.Module):
         self.up3 = Up(256, 64)
         self.up4 = Up(128, 64)
         self.outc = OutConv(64, classes)
-        
 
     def forward(self, x):
         x1 = self.inc(x)
