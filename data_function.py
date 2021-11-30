5d659f20da21c036710081f0dee56d6e3ca89274
pyh-pyh <2335257900@qq.com>
2021-11-30 15:24:41 +0800

    formatting

--- data_function.py
+++ data_function.py
@@ -1,6 +1,7 @@
 from glob import glob
 from os.path import dirname, join, basename, isfile
 import sys
+
 sys.path.append('./')
 import csv
 import torch
@@ -49,17 +50,17 @@ class MedData_train(torch.utils.data.Dataset):
         queue_length = 5
         samples_per_volume = 5
 
-
         self.subjects = []
 
-        if (hp.in_class == 1) and (hp.out_class == 1) :
+        if (hp.in_class == 1) and (hp.out_class == 1):
 
             images_dir = Path(images_dir)
             self.image_paths = sorted(images_dir.glob(hp.fold_arch))
             labels_dir = Path(labels_dir)
             self.label_paths = sorted(labels_dir.glob(hp.fold_arch))
 
-            for (image_path, label_path) in zip(self.image_paths, self.label_paths):
+            for (image_path, label_path) in zip(self.image_paths,
+                                                self.label_paths):
                 subject = tio.Subject(
                     source=tio.ScalarImage(image_path),
                     label=tio.LabelMap(label_path),
@@ -69,20 +70,25 @@ class MedData_train(torch.utils.data.Dataset):
             images_dir = Path(images_dir)
             self.image_paths = sorted(images_dir.glob(hp.fold_arch))
 
-            artery_labels_dir = Path(labels_dir+'/artery')
-            self.artery_label_paths = sorted(artery_labels_dir.glob(hp.fold_arch))
+            artery_labels_dir = Path(labels_dir + '/artery')
+            self.artery_label_paths = sorted(
+                artery_labels_dir.glob(hp.fold_arch))
 
-            lung_labels_dir = Path(labels_dir+'/lung')
+            lung_labels_dir = Path(labels_dir + '/lung')
             self.lung_label_paths = sorted(lung_labels_dir.glob(hp.fold_arch))
 
-            trachea_labels_dir = Path(labels_dir+'/trachea')
-            self.trachea_label_paths = sorted(trachea_labels_dir.glob(hp.fold_arch))
+            trachea_labels_dir = Path(labels_dir + '/trachea')
+            self.trachea_label_paths = sorted(
+                trachea_labels_dir.glob(hp.fold_arch))
 
-            vein_labels_dir = Path(labels_dir+'/vein')
+            vein_labels_dir = Path(labels_dir + '/vein')
             self.vein_label_paths = sorted(vein_labels_dir.glob(hp.fold_arch))
 
-
-            for (image_path, artery_label_path,lung_label_path,trachea_label_path,vein_label_path) in zip(self.image_paths, self.artery_label_paths,self.lung_label_paths,self.trachea_label_paths,self.vein_label_paths):
+            for (image_path, artery_label_path, lung_label_path,
+                 trachea_label_path, vein_label_path) in zip(
+                     self.image_paths, self.artery_label_paths,
+                     self.lung_label_paths, self.trachea_label_paths,
+                     self.vein_label_paths):
                 subject = tio.Subject(
                     source=tio.ScalarImage(image_path),
                     atery=tio.LabelMap(artery_label_path),
@@ -92,11 +98,10 @@ class MedData_train(torch.utils.data.Dataset):
                 )
                 self.subjects.append(subject)
 
-
         self.transforms = self.transform()
 
-        self.training_set = tio.SubjectsDataset(self.subjects, transform=self.transforms)
-
+        self.training_set = tio.SubjectsDataset(self.subjects,
+                                                transform=self.transforms)
 
         self.queue_dataset = Queue(
             self.training_set,
@@ -105,72 +110,68 @@ class MedData_train(torch.utils.data.Dataset):
             UniformSampler(patch_size),
         )
 
-
-
-
     def transform(self):
 
         if hp.mode == '3d':
             if hp.aug:
                 training_transform = Compose([
-                # ToCanonical(),
-                CropOrPad((hp.crop_or_pad_size), padding_mode='reflect'),
-                # RandomMotion(),
-                RandomBiasField(),
-                ZNormalization(),
-                RandomNoise(),
-                RandomFlip(axes=(0,)),
-                OneOf({
-                    RandomAffine(): 0.8,
-                    RandomElasticDeformation(): 0.2,
-                }),])
+                    # ToCanonical(),
+                    CropOrPad((hp.crop_or_pad_size), padding_mode='reflect'),
+                    # RandomMotion(),
+                    RandomBiasField(),
+                    ZNormalization(),
+                    RandomNoise(),
+                    RandomFlip(axes=(0, )),
+                    OneOf({
+                        RandomAffine(): 0.8,
+                        RandomElasticDeformation(): 0.2,
+                    }),
+                ])
             else:
                 training_transform = Compose([
-                CropOrPad((hp.crop_or_pad_size), padding_mode='reflect'),
-                ZNormalization(),
+                    CropOrPad((hp.crop_or_pad_size), padding_mode='reflect'),
+                    ZNormalization(),
                 ])
         elif hp.mode == '2d':
             if hp.aug:
                 training_transform = Compose([
-                CropOrPad((hp.crop_or_pad_size), padding_mode='reflect'),
-                # RandomMotion(),
-                RandomBiasField(),
-                ZNormalization(),
-                RandomNoise(),
-                RandomFlip(axes=(0,)),
-                OneOf({
-                    RandomAffine(): 0.8,
-                    RandomElasticDeformation(): 0.2,
-                }),])
+                    CropOrPad((hp.crop_or_pad_size), padding_mode='reflect'),
+                    # RandomMotion(),
+                    RandomBiasField(),
+                    ZNormalization(),
+                    RandomNoise(),
+                    RandomFlip(axes=(0, )),
+                    OneOf({
+                        RandomAffine(): 0.8,
+                        RandomElasticDeformation(): 0.2,
+                    }),
+                ])
             else:
                 training_transform = Compose([
-                CropOrPad((hp.crop_or_pad_size), padding_mode='reflect'),
-                ZNormalization(),
+                    CropOrPad((hp.crop_or_pad_size), padding_mode='reflect'),
+                    ZNormalization(),
                 ])
 
         else:
             raise Exception('no such kind of mode!')
 
-
         return training_transform
-
-
 
 
 class MedData_test(torch.utils.data.Dataset):
     def __init__(self, images_dir, labels_dir):
 
-
         self.subjects = []
 
-        if (hp.in_class == 1) and (hp.out_class == 1) :
+        if (hp.in_class == 1) and (hp.out_class == 1):
 
             images_dir = Path(images_dir)
             self.image_paths = sorted(images_dir.glob(hp.fold_arch))
             labels_dir = Path(labels_dir)
             self.label_paths = sorted(labels_dir.glob(hp.fold_arch))
 
-            for (image_path, label_path) in zip(self.image_paths, self.label_paths):
+            for (image_path, label_path) in zip(self.image_paths,
+                                                self.label_paths):
                 subject = tio.Subject(
                     source=tio.ScalarImage(image_path),
                     label=tio.LabelMap(label_path),
@@ -180,20 +181,25 @@ class MedData_test(torch.utils.data.Dataset):
             images_dir = Path(images_dir)
             self.image_paths = sorted(images_dir.glob(hp.fold_arch))
 
-            artery_labels_dir = Path(labels_dir+'/artery')
-            self.artery_label_paths = sorted(artery_labels_dir.glob(hp.fold_arch))
+            artery_labels_dir = Path(labels_dir + '/artery')
+            self.artery_label_paths = sorted(
+                artery_labels_dir.glob(hp.fold_arch))
 
-            lung_labels_dir = Path(labels_dir+'/lung')
+            lung_labels_dir = Path(labels_dir + '/lung')
             self.lung_label_paths = sorted(lung_labels_dir.glob(hp.fold_arch))
 
-            trachea_labels_dir = Path(labels_dir+'/trachea')
-            self.trachea_label_paths = sorted(trachea_labels_dir.glob(hp.fold_arch))
+            trachea_labels_dir = Path(labels_dir + '/trachea')
+            self.trachea_label_paths = sorted(
+                trachea_labels_dir.glob(hp.fold_arch))
 
-            vein_labels_dir = Path(labels_dir+'/vein')
+            vein_labels_dir = Path(labels_dir + '/vein')
             self.vein_label_paths = sorted(vein_labels_dir.glob(hp.fold_arch))
 
-
-            for (image_path, artery_label_path,lung_label_path,trachea_label_path,vein_label_path) in zip(self.image_paths, self.artery_label_paths,self.lung_label_paths,self.trachea_label_paths,self.vein_label_paths):
+            for (image_path, artery_label_path, lung_label_path,
+                 trachea_label_path, vein_label_path) in zip(
+                     self.image_paths, self.artery_label_paths,
+                     self.lung_label_paths, self.trachea_label_paths,
+                     self.vein_label_paths):
                 subject = tio.Subject(
                     source=tio.ScalarImage(image_path),
                     atery=tio.LabelMap(artery_label_path),
@@ -203,11 +209,6 @@ class MedData_test(torch.utils.data.Dataset):
                 )
                 self.subjects.append(subject)
 
-
         # self.transforms = self.transform()
 
         self.training_set = tio.SubjectsDataset(self.subjects, transform=None)
-
-
-
-
